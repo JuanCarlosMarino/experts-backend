643d17add121b1c110288b80039a2d7449cfdfde
Juan Carlos Mariño Morantes <46898982+JuanCarlosMarino@users.noreply.github.com>
2021-11-29 18:45:11 -0500

    firestore key

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./experts-81553-e5f9c727d839.json');
+const serviceAccount = require('./experts2-38bfa20fceb1.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
